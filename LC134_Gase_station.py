df834962388183f6880411c150fbdd019d087072
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-06-14 22:23:55 -0400

    Create LC134_Gase_station.py

---
+++ LC134_Gase_station.py
@@ -0,0 +1,78 @@
+"""
+
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+ 
+
+Example 1:
+
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+Example 2:
+
+Input: gas = [2,3,4], cost = [3,4,3]
+Output: -1
+Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start.
+ 
+
+Constraints:
+
+n == gas.length == cost.length
+1 <= n <= 105
+0 <= gas[i], cost[i] <= 104
+
+"""
+
+
+def can_complete_circuit(gas, cost):
+    # Initiate the required variables.
+    total_tank, curr_tank = 0, 0
+    starting_station = 0
+
+    # Iterate over all the stations
+    for i, val in enumerate(gas):
+        # update the varaibles at each step
+        total_tank += val - cost[i]
+        curr_tank += val - cost[i]
+
+        # If we cannot reach the current station
+        if curr_tank < 0:
+
+            # Make the next station as a new starting point and reset the tank to 
+            #start with an empty tank.
+            starting_station = i + 1
+
+            # Start with an empty tank.
+            curr_tank = 0
+    
+    return starting_station if total_tank >= 0 else -1
+
+# Driver Code
+
+gas = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+index = can_complete_circuit(gas, cost)
+
+if index == -1:
+    print("Output: "+ str(index))
+    print("We cannot make the round trip once, no matter where you start.")
+else:
+    print("Starting gas station's index is: " + str(index) + ".")

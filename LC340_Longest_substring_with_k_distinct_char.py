2cd538646b6e145e7dfc9afe99d9c9a1333b2046
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-06-18 14:12:53 -0400

    Create LC340_Longest_substring_with_k_distinct_char.py

---
+++ LC340_Longest_substring_with_k_distinct_char.py
@@ -0,0 +1,54 @@
+"""
+Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+ 
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: The substring is "ece" with length 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: The substring is "aa" with length 2.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+0 <= k <= 50
+"""
+
+## APPROACH 1:
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+        if k == 0 or len(s)==0 :
+            return 0
+        left = 0
+        right = 0
+        current_map = dict()
+        max_length = 0
+        
+        
+        while right < len(s):
+            
+            rchar = s[right]
+            if rchar in current_map:
+                current_map[rchar]+=1
+            else:
+                # add it to map
+                current_map[rchar] = 1
+                while len(current_map)>k:
+                    lchar = s[left]
+                    current_map[lchar]-=1
+                    if current_map[lchar]==0:
+                        del current_map[lchar]
+                    left+=1
+            max_length = max(max_length, right-left+1)
+            
+            right+=1
+        return max_length

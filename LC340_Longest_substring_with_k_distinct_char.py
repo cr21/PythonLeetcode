971376221d7fc81a09ddb16236b6094623cbb9f6
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-06-18 14:22:13 -0400

    Update LC340_Longest_substring_with_k_distinct_char.py

--- LC340_Longest_substring_with_k_distinct_char.py
+++ LC340_Longest_substring_with_k_distinct_char.py
@@ -52,3 +52,41 @@ class Solution:
             
             right+=1
         return max_length
+       
+       
+       
+# APPROACH 2
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+        if k == 0 or len(s)==0 :
+            return 0
+        left = 0
+        right = 0
+        start = 0
+        end = 0
+        character_map = defaultdict() # character -> its rightmost position
+        """
+        Maintain the Character map with it's right most position for K distinct values
+        Once map size is k+1, remove the character having minimum right most value
+        and put left pointer at that position +1
+        if current window is larger than previous best update it
+        and return it
+        
+        
+        """
+        while right < len(s):
+            
+            character_map[s[right]]=right
+            right+=1
+            if len(character_map) == k+1:
+                min_idx_char = min(character_map.values())
+                del character_map[s[min_idx_char]]
+                left= min_idx_char+1
+            if (right-left) > (end-start):
+                end = right
+                start = left
+                
+        return len(s[start:end])
+            

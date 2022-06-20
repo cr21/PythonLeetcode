90b57f292ae9d25ed6024f29ec7da404726d7d3c
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-06-20 00:36:01 -0400

    Update LC647Pallindromic_substring.py

--- LC647Pallindromic_substring.py
+++ LC647Pallindromic_substring.py
@@ -20,18 +20,42 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
  
 
 """
-
-## BRUTE FORCE
-
 class Solution:
     def countSubstrings(self, s: str) -> int:
         
-        # Brute Force
-        count=0
-        # iterate over every substring possible and check if they are pallindromic or not
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                subs = s[i:j+1]
-                if subs == subs[::-1]:
-                    count+=1
-        return count
+        # # Brute Force
+        # count=0
+        # # iterate over every substring possible and check if they are pallindromic or not
+        # for i in range(len(s)):
+        #     for j in range(i,len(s)):
+        #         subs = s[i:j+1]
+        #         if subs == subs[::-1]:
+        #             count+=1
+        # return count
+        
+        
+        # Expand around center
+        
+        ans = 0
+        
+        def countPallindrome(s, left, right):
+            res = 0
+            # keep on decrementing left and incrementing right till both left and right
+            # index value are same denoting pallindromic behaviour
+            # and we increment count by 1 every time condition based on how many times while loop run
+            while left >=0 and right < len(s) :
+                if(s[left]!=s[right]):
+                    break
+                left-=1
+                right+=1
+                res+=1
+            return res
+            
+        
+            
+        for index in range(len(s)):
+            ans+= countPallindrome(s,index,index)
+            ans+=countPallindrome(s, index, index+1)
+        
+        return ans
+    

4288c505ea78307f6287abd351b1649f8afe3cc1
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-07-07 04:51:10 -0400

    Update LC_227_Basic_calculator.py

--- Stack/LC_227_Basic_calculator.py
+++ Stack/LC_227_Basic_calculator.py
@@ -76,3 +76,49 @@ class Solution:
             
         result = 0
         return sum(stack)
+       
+       
+       ## APPROACH 2 no STACK
+       
+       length = len(s)
+        if length==0:
+            return 0
+        
+        last_number = 0
+        result = 0
+        
+        current_number=0
+        # keep track of which operation to perform on current number
+        # by default it will be addition
+        operation = '+'
+        
+        index = 0
+        
+        while index < length:
+            curr_char = s[index]
+            
+            if curr_char.isdigit():
+                current_number = (current_number*10)+(ord(curr_char)-ord('0'))
+            
+            if not curr_char.isdigit() and not curr_char.isspace() or index==length-1:
+                if operation =='-' or  operation =='+':
+                    result+=last_number
+                    if (operation == '+'):
+                        last_number = current_number   
+                    else:
+                        last_number = -current_number
+                elif operation =='*':
+                   
+                    last_number = last_number * current_number
+                elif operation =='/':
+
+                    last_number = int(last_number / current_number)
+                operation = curr_char
+                current_number = 0
+                
+            index+=1
+            
+        result+=last_number
+            
+            
+        return result

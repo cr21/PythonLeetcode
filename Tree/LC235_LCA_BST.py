a5ce4e65b0b4cb7df800c103d3b1864495719038
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-07-27 03:09:00 -0400

    Update LC235_LCA_BST.py

--- Tree/LC235_LCA_BST.py
+++ Tree/LC235_LCA_BST.py
@@ -42,41 +42,61 @@ p and q will exist in the BST.
 #         self.val = x
 #         self.left = None
 #         self.right = None
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if not root :
-            return None
+#         if not root :
+#             return None
         
-        # if we ever found any of the node p or q return it
-        # as we know we reach any of the node , other node should be in descendent of that root
-        if root.val == p.val or root.val == q.val:
-            return root
+#         # if we ever found any of the node p or q return it
+#         # as we know we reach any of the node , other node should be in descendent of that root
+#         if root.val == p.val or root.val == q.val:
+#             return root
         
-        p_left=False
-        q_left = False
+#         p_left=False
+#         q_left = False
         
-        if p.val <  root.val:
-            p_left = True
+#         if p.val <  root.val:
+#             p_left = True
             
-        if q.val < root.val:
-            q_left = True
-        # if q and p are different side of current root return current root
-        if (q_left and not p_left) or (p_left and not q_left) :
-            return root
+#         if q.val < root.val:
+#             q_left = True
+#         # if q and p are different side of current root return current root
+#         if (q_left and not p_left) or (p_left and not q_left) :
+#             return root
         
-        # if both p and q are left side of current root
-        # return lowest lca of current root left subtree
-        if p_left and q_left:
-            return  self.lowestCommonAncestor(root.left, p,q)
-        # if both p and q are right side of current root
-        # return lowest lca of current root right subtree
-        if not p_left and  not q_left:
-            return  self.lowestCommonAncestor(root.right, p,q)
+#         # if both p and q are left side of current root
+#         # return lowest lca of current root left subtree
+#         if p_left and q_left:
+#             return  self.lowestCommonAncestor(root.left, p,q)
+#         # if both p and q are right side of current root
+#         # return lowest lca of current root right subtree
+#         if not p_left and  not q_left:
+#             return  self.lowestCommonAncestor(root.right, p,q)
         
-        return root
+#         return root
         
         
+        #  approach 2
         
-
+        curr = root
+        
+        while curr:
+            # If both P and Q are on right side of tree
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            # If both P and Q are on Left side of tree
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
+        return None
+        
+        

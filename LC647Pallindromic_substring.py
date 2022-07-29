f699b8ab26c0c558914db52952cf5e9c5dcdf043
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-07-28 21:51:38 -0400

    Update LC647Pallindromic_substring.py

--- LC647Pallindromic_substring.py
+++ LC647Pallindromic_substring.py
@@ -43,9 +43,7 @@ class Solution:
             # keep on decrementing left and incrementing right till both left and right
             # index value are same denoting pallindromic behaviour
             # and we increment count by 1 every time condition based on how many times while loop run
-            while left >=0 and right < len(s) :
-                if(s[left]!=s[right]):
-                    break
+            while left >=0 and right < len(s)  and s[left]==s[right]:
                 left-=1
                 right+=1
                 res+=1
@@ -59,3 +57,24 @@ class Solution:
         
         return ans
     
+
+#         total = 0 
+#         def countHelper(s,left, right):
+#             cnt = 0
+#             while left >=0 and right < len(s) and s[left] == s[right]:
+#                 left-=1
+#                 right+=1
+#                 cnt+=1
+                
+#             return cnt
+                
+#         for index in range(len(s)):
+#             # expand around cennter location index and return all the odd length pallindromic
+#             # substring which has middle index 
+#             total+= countHelper(s, index, index)
+#             # expand around cennter location index and return all the even length pallindromic
+#             # substring which has middle index 
+#             total += countHelper(s, index, index+1)
+            
+#         return total
+        

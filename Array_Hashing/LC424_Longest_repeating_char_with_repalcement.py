ec8b74d07bc1d4403755d997dae71feea9640674
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-08-24 01:51:09 -0400

    Update LC424_Longest_repeating_char_with_repalcement.py

--- Array_Hashing/LC424_Longest_repeating_char_with_repalcement.py
+++ Array_Hashing/LC424_Longest_repeating_char_with_repalcement.py
@@ -58,6 +58,64 @@ class Solution:
             right+=1
             
         return max_len
+       
+       
+       
+       class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        # APPROACH 1 :O(N)
+#         left = 0
+#         right = 0
+#         max_len = 0
+#         current_window_best=0
+#         current_window_length = 0
+#         counter = {}
+#         # A A B A B B B
+        
+#         while right < len(s):
+#             ch = s[right]
+#             counter[ch] = counter.get(ch,0)+1
+#             # which char has maximum frequency in current windo
+#             current_window_best = max(current_window_best, counter.get(ch))
+            
+#             # update current window length
+#             current_window_length = right-left+1
+            
+#             # if there are some room for character to change then change it
+            
+#             if current_window_length - current_window_best > k :
+#                 left_ch = s[left]
+#                 counter[left_ch] = counter.get(left_ch)-1
+#                 left+=1
+                
+#             max_len = max(max_len,right-left+1)
+            
+#             right+=1
+            
+#         return max_len
+
+        # APPROACH 2 O(26*N)
+        
+        left = 0
+        res=0
+        counter={}
+        max_freq_element = 0
+        
+        for right in range(len(s)):
+            rchar = s[right]
+            counter[rchar]= 1 + counter.get(rchar,0)
+            
+            # maximum replacement that could have been allowed
+            while  (right-left+1) - max(counter.values())  > k :
+                lchar = s[left]
+                counter[lchar]-=1
+                
+                left+=1
+                
+            res = max(res, right-left+1)
+            
+        return res
                 
         
         

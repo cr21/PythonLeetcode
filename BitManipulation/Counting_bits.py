af35bf78a75ea069efdc5f98e2f39f631e0e034c
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-08-25 01:57:00 -0400

    Update Counting_bits.py

--- BitManipulation/Counting_bits.py
+++ BitManipulation/Counting_bits.py
@@ -37,22 +37,53 @@ Can you do it without using any built-in function (i.e., like __builtin_popcount
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def count_bits(n):
-            mask = 1
-            count = 0
-            for index in range(32):
-                # for every position slide masking variable and take and 
-                if n & mask != 0 :
-                    count+=1
-                # left shift the mask bit 
-                mask = mask << 1
+        
+        # APPROACH 1
+#         def count_bits(n):
+#             mask = 1
+#             count = 0
+#             for index in range(32):
+#                 # for every position slide masking variable and take and 
+#                 if n & mask != 0 :
+#                     count+=1
+#                 # left shift the mask bit 
+#                 mask = mask << 1
                 
-            return count
+#             return count
     
-        ans = []
-        for num in range(n+1):
-            ans.append(count_bits(num))
-        return ans
+#         ans = []
+#         for num in range(n+1):
+#             ans.append(count_bits(num))
+#         return ans
+
+#         # APPRAOCH 2
+    
+#         def popCount(x):
+#             count = 0
+#             while x != 0:
+#                 # e.g x= 3 = 0011  and x-1 == 2 == 0010
+#                 # 3 & 2 = 0011 & 0010 = 0010 
+#                 # 2 & 1 = 0010 & 0001 = 0000
+#                 x &= x-1 # zero out least significant digit
+#                 count+=1
+#             return count
+        
+#         ans = [0]*(n+1)
+        
+#         for num in range(n+1):
+#             ans[num] = popCount(num)
+            
+#         return ans
+
+        # APPRAOCH 3 DP + Least Significant Bit
+        dp = [0]*(n+1)
+        offset =1
+        for x in range(1, n+1):
+            if offset*2==x:
+                offset=x
+            dp[x]=1+dp[x-offset]
+        return dp
+            
 
     
         
